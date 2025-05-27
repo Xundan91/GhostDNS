f8282eaf71d2eaa1086f528c0b0a631cac6abe62
Xundan91 <kundansingh023230@gmail.com>
2025-05-27 23:03:58 +0530

    [auth setup]

--- app/api/auth/[nextauth]/route.ts
+++ app/api/auth/[nextauth]/route.ts
@@ -1,9 +1,32 @@
 import NextAuth from "Next-Auth"
+import CredentialsProvider  from "next-auth/providers/credentials"
 import GoogleProvider from "next-auth/providers/google"
 // import VercelProvider from "next-auth/providers/vercel"
 import NetlifyProvider from "next-auth/providers/netlify"
 
 
-export default NextAuth({
+const handler =  NextAuth({
+providers:[
+    CredentialsProvider({
+        name : "Credentials",
+        credentials :{
+            email:{label : "Email", type : "email"},
+            password : {label : "password ",type : "password"},
+        }, 
+        async authorize(credentials){
+            if(!credentials?.email || !credentials?.password) return null;
 
+            try {
+                return {
+                  
+
+                }
+                
+            } catch (error) {
+                
+            }
+
+        }
+    })
+]
 })
