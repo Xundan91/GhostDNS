7d171b8b1766ec3f84add42c8f8ed690197c7ab3
Xundan91 <kundansingh023230@gmail.com>
2025-05-27 22:41:53 +0530

    fix: Auth

---
+++ app/api/auth/[nextauth]/route.ts
@@ -0,0 +1,9 @@
+import NextAuth from "Next-Auth"
+import GoogleProvider from "next-auth/providers/google"
+// import VercelProvider from "next-auth/providers/vercel"
+import NetlifyProvider from "next-auth/providers/netlify"
+
+
+export default NextAuth({
+
+})
