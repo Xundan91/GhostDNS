38822c01bf0735fded69b67171fec92ee3c627d8
Xundan91 <kundansingh023230@gmail.com>
2025-05-29 17:32:26 +0530

    added

--- app/api/auth/[nextauth]/route.ts
+++ app/api/auth/[nextauth]/route.ts
@@ -1,3 +1,34 @@
-import { handlers } from "@/auth";
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from "next-auth";
+import type { NextAuthConfig } from "next-auth";
+import { OAuthConfig } from "next-auth/providers";
 
-export const { GET, POST } = handlers;
+const VercelProvider = {
+  id: "vercel",
+  name: "Vercel",
+  type: "oauth",
+  authorization: {
+    url: "https://vercel.com/oauth/authorize",
+    params: { scope: "read write" },
+  },
+  token: "https://api.vercel.com/v2/oauth/access_token",
+  userinfo: "https://api.vercel.com/v2/user",
+  clientId: process.env.VERCEL_CLIENT_ID!,
+  clientSecret: process.env.VERCEL_CLIENT_SECRET!,
+  profile(profile) {
+    return {
+      id: profile.user.id,
+      name: profile.user.name,
+      email: profile.user.email,
+      image: profile.user.avatar,
+    };
+  },
+} satisfies OAuthConfig<any>;
+
+export const authOptions: NextAuthConfig = {
+  providers: [VercelProvider],
+  secret: process.env.AUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { auth } from "@/auth";
-
-export default auth((req) => {
-});
-
-export const config = {
-  matcher: ["/dashboard/:path*"], // Adjust this pattern to match your protected routes
-};
