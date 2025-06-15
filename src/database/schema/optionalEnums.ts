6ca8ee5a82fee395d3f510bdfa82e71ea0a0fba8
Xundan91 <kundansingh023230@gmail.com>
2025-06-15 22:09:55 +0530

    Fixed bug

--- src/database/schema/optionalEnums.ts
+++ src/database/schema/optionalEnums.ts
@@ -1,4 +1,4 @@
-// export const platformEnum = ["HOSTINGER", "GODADDY", "CLOUDFLARE", "SQUARESPACE"] as const
-// export const dnsStatusEnum = ["PENDING", "VERIFIED", "FAILED"] as const
-// export const purchaseStatusEnum = ["PENDING", "COMPLETED", "FAILED"] as const
-// export const userRoleEnum = ["USER", "ADMIN"] as const
+export const platformEnum = ["HOSTINGER", "GODADDY", "CLOUDFLARE", "SQUARESPACE"] as const
+export const dnsStatusEnum = ["PENDING", "VERIFIED", "FAILED"] as const
+export const purchaseStatusEnum = ["PENDING", "COMPLETED", "FAILED"] as const
+export const userRoleEnum = ["USER", "ADMIN"] as const
