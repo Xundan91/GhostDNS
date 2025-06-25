3d0cfc6cbc1b091264f7a851700fd25ce254327d
xundan91 <kundansingh023230@gmail.com>
2025-06-25 22:56:54 +0530

    [updated the vercel log]

--- src/app/api/services/platform/vercel/route.ts
+++ src/app/api/services/platform/vercel/route.ts
@@ -1,22 +1,46 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
+import { Vercel } from '@vercel/sdk';
+//token
+//domain
+//subdomain
+//projectname
 
+const vercel = new Vercel(
+  {
+    bearerToken : "9ZVxALTbQiCNGJNPKES77eXS",
+  }
+)
 export async function POST(req: Request) {
   try {
-    const { apiKey } = await req.json();
-    if (!apiKey) {
-      return NextResponse.json({ error: 'Missing Vercel API key' }, { status: 400 });
+    const {subdomain} =  await req.json();
+    if(!subdomain){
+      return NextResponse.json({message : "missing subdomain"},
+      {
+        status : 400
+      }
+    )
     }
-    const res = await fetch('https://api.vercel.com/v9/projects', {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    });
-    if (!res.ok) {
-      const error = await res.json();
-      return NextResponse.json({ error: error.error?.message || 'Failed to fetch projects from Vercel' }, { status: res.status });
-    }
-    const data = await res.json();
-    return NextResponse.json({ projects: data.projects || [] });
+    const fulldomain = `${subdomain}.xundan.in`
+    try {
+      const vercelResponse = await vercel.projects.addProjectDomain(
+        {
+          idOrName : "micro-tech",
+          requestBody: {
+            name: fulldomain,
+          },
+        }
+      )
+      return NextResponse.json({
+        message : `Domain is register succesfully  ${fulldomain} `,
+        data : vercelResponse
+      });
+      
+    } catch (error: any) {
+      console.error("error adding domain to vercel", error.NextResponse?.data || error.message)
+      return NextResponse.json({
+        message : " There is error in setting the domain "
+      })
+    } 
   } catch (error) {
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
   }
