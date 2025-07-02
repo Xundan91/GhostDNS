529f1fe8ec7dadc3b418ffe25156e44aef5be714
xundan91 <kundansingh023230@gmail.com>
2025-07-02 22:20:20 +0530

    small update

--- src/database/schema/basedomain.ts
+++ src/database/schema/basedomain.ts
@@ -3,13 +3,14 @@ import {sql} from "drizzle-orm"
 import {users} from "./users"
 export const basedomain = pgTable("basedomain",{
     id: text("id").primaryKey().default(sql`gen_random_uuid()`),
+    ownerId : text("owner_id").notNull().references(()=>users.id,{
+        onDelete:"cascade"
+    }),
     domainName : text("domain_name").notNull().unique(),
     platform: text("platform").notNull(),
     price: text("price").notNull().default("0"), 
     apiKey: text("api_key"),     ///hash in nexxt version   
-    ownerId : text("owner_id").notNull().references(()=>users.id,{
-        onDelete:"cascade"
-    }),
+    
     createdAt : timestamp("created_at", {withTimezone:true}).defaultNow(),
     updatedAt : timestamp("updated_At", {withTimezone:true}).defaultNow()
     
