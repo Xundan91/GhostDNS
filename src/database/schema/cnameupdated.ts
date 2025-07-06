03d3505d3dc29b0205a3e6c4f71aa1bbc9e8825a
xundan91 <kundansingh023230@gmail.com>
2025-07-06 13:37:29 +0530

    [Added] : schema updated connectproject and cnameupdate

---
+++ src/database/schema/cnameupdated.ts
@@ -0,0 +1,16 @@
+import { pgTable,uuid,text,varchar, timestamp } from "drizzle-orm/pg-core";
+import {users} from "./users"
+import { basedomain } from "./basedomain";
+import { purchase } from "./purchase";
+import { connectproject } from "./connectproject";
+export const cnameupdate = pgTable("cnameupdate",{
+    id : uuid("id").primaryKey().defaultRandom(),
+    connectedproject : text("connectedproject").notNull().references(()=>connectproject.id),
+    userid : text("userid").notNull().references(()=>users.id),
+    basedomain: text("userid").notNull().references(()=>basedomain.id),
+    purchasedomain : text("purchasedomain").notNull().references(()=>purchase.id),
+    cname : varchar("cname",{length: 32}).notNull(),
+    deployedurl: varchar("deployedurl", {length:256}).notNull(),
+    createdAt : timestamp("createdAt").defaultNow().notNull(),
+    updatedAt : timestamp("UpdatedAt").defaultNow().notNull()
+})
---
+++ src/database/schema/connectproject.ts
@@ -0,0 +1,20 @@
+import { pgTable , text , uuid, varchar,timestamp, boolean } from "drizzle-orm/pg-core";
+import {sql} from "drizzle-orm"
+import {users} from "./users"
+import { basedomain } from "./basedomain";
+import { purchase } from "./purchase";
+export const connectproject = pgTable("connectproject", {
+    id : uuid("id").primaryKey().defaultRandom(),
+    isconnected : boolean("isconnected").default(true),
+    userId : text("userId").references(()=>users.id),
+    basedomain : text("basedomain").notNull().references(()=>basedomain.id),
+    purchasedomain : text("purchasedomain").notNull().references(()=>purchase.id),
+    platform :varchar("platform", {length : 32}).notNull(),
+    connectApiKey : varchar("connectApiKey",{length:256}),
+    project_id : varchar("project_id", {length : 256}),
+    project_name : varchar("project_name", {length : 256}),
+    deployed_url : varchar("deployed_url", {length : 512}),
+    created_at : timestamp("createdAt").defaultNow().notNull(),
+    updated_at : timestamp("updatedAt").defaultNow().notNull()
+
+}) 
